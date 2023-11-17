59ab9e44715d06e5c339564546f6488001c1709d
Gabriel Rovesti <rovestigabriel@gmail.com>
2023-11-17 19:10:52 +0100

    Aggiunti ulteriori commenti agli esercizi risolti

--- Tutorato 3 - 15.11/Soluzioni/Es2.cpp
+++ Tutorato 3 - 15.11/Soluzioni/Es2.cpp
@@ -30,3 +30,17 @@ int main(){
 Stampa: 4 3.1 - istanziazione implicita fun<int>
 Stampa: blob 3.1 - istanzazione implicita fun<string>
 */
+
+/*
+Sostituisci l'int e quindi diventa (4), poi subito dopo hai C<double> che nuovamente va la stessa chiamata e il template <T> diventa t.x (4) e c.x (altra variabile sostituita con 3.1) e infatti è 4 (prima sostituisci "int") e poi 3.1 (perché <double> è <T>)
+
+La class V semplicemente dice "ti faccio vedere i campi privati di C", quindi il tipo sottostante "int/string" semplicemente sostituisce quel tipo e permette la chiamata della funzione con il semplice "fun"
+
+Appunto:
+- c<int> c(4) -> stampa 4 essendo che sostituisce il template
+- c.x (è c<double> e stampa 3.1)
+
+Successivamente:
+- t.x (diventa <string> e quindi stampa "blob"
+- c.x (diventa <double> e nuovamente 3.1)
+*/
--- Tutorato 3 - 15.11/Soluzioni/Es3.cpp
+++ Tutorato 3 - 15.11/Soluzioni/Es3.cpp
@@ -49,3 +49,20 @@ Non compila
 Non compila
 Compila
 */
+
+/*
+Il fatto che compili dipende essenzialmente dal tipo:
+- tu puoi vedere che <char> per la classe D
+- la classe C prende le invocazioni dei metodi
+- puoi vedere che C è friend della classe T1; quindi, se i tipi di T1 fanno match con C, compila
+
+Tu hai il fatto di "char" che deve compilare a seconda dei due tipi con cui viene chiamata fun. Quindi, compila se <T> o <char> è il primo parametro, mentre se è un tipo diverso, non va.
+
+Esempio: 
+<T, T> - compila, perché char va sempre bene per T, a prescindere dal secondo parametro
+<int, T> - non compila, perché char non è di tipo int ed essendo tipo diverso non fanno match
+<T, int> - compila, stesso motivo della prima
+<int, int> - non compila, stesso motivo di <int, T>
+<int, double> - non compila, stesso motivo di <int, T>
+<char, double> - compila, fa match con char
+*/
